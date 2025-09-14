97370017785827818a825f454cae22637fbcea7d
Ibrahim Adewale Adeniran <91672319+DIFoundation@users.noreply.github.com>
2025-09-14 21:33:30 +0100

    Update wagmi-config.ts

--- frontend/lib/wagmi-config.ts
+++ frontend/lib/wagmi-config.ts
@@ -1,9 +1,10 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
-import { mainnet, polygon, optimism, arbitrum, base, sepolia, celo, celoAlfajores, celoSepolia } from 'wagmi/chains'
+import { getDefaultConfig, htpp } from '@rainbow-me/rainbowkit'
+import { celoSepolia } from 'wagmi/chains'
 
 export const config = getDefaultConfig({
   appName: 'Tixora - Decentralized Event Ticketing',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '2f05a7cac472734e5e09745c8b4a9c5e', // Use a valid project ID
-  chains: [celoSepolia, celo, celoAlfajores, mainnet, polygon, optimism, arbitrum, base, sepolia],
+  chains: [celoSepolia],
+  transport: [http('https://rpc.ankr.com/celo_sepolia')]
   ssr: true, // If your dApp uses server side rendering (SSR)
 })
