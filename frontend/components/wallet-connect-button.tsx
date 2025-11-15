9ef1b5763e1b59e354ccd483e4bdd88af0ffef4c
DIFoundation <adeniranibrahim165@gmail.com>
2025-11-15 08:08:22 +0100

    .

--- frontend/components/wallet-connect-button.tsx
+++ frontend/components/wallet-connect-button.tsx
@@ -3,27 +3,16 @@
 import {
   AppKitButton,
   AppKitNetworkButton,
+  AppKitAccountButton,
 } from "@reown/appkit/react";
 
 export function WalletConnectButton() {
 
   return (
     <div className="flex gap-4">
-      {/* <AppKitButton 
-        balance="hide" 
-        className="bg-gray-200 px-3 py-2 rounded-lg text-gray-900"
-      />
-      
-      <AppKitNetworkButton
-        className="bg-gray-200 px-3 py-2 rounded-lg text-gray-900"
-      /> */}
-
-
-      <appkit-account-button />
-      <appkit-button />
-      <appkit-connect-button />
-      <appkit-modal />
-      <appkit-network-button />
+      <AppKitButton />
+      <AppKitNetworkButton />
+      <AppKitAccountButton />
     </div>
   );
 }
